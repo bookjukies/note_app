d1f2c54d39a104b24bc5a8ad2f0ebb469686d251
Bongani <mkhumbie34@gmail.com>
2022-09-05 13:56:10 +0200

    final before review by client

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const drag = document.querySelector('.drag');
-const note_app = document.querySelector(`.container`);
-const maximize_button = document.querySelector(`.closed`);
-const minimize_button = document.querySelector(`.minimize`);
-const add_button = document.querySelector(`.add`);
-const note = document.querySelector(`.note`);
-const save_buttons = note.querySelectorAll(`buttons`); //adding anothor button type is a !!!BREAKING CHANGE!!!(currently only has save button)
+const drag = document.querySelector('.qn-drag');
+const note_app = document.querySelector(`.qn-container`);
+const maximize_button = document.querySelector(`.qn-closed`);
+const minimize_button = document.querySelector(`.qn-minimize`);
+const add_button = document.querySelector(`.qn-add`);
+const note = document.querySelector(`.qn-note`);
 
 // display
-maximize_button.classList.add(`none`);
+maximize_button.classList.add(`qn-none`);
 // minimize
 minimize_button.addEventListener(`click`, () => {
-  note_app.classList.add(`none`);
-  maximize_button.classList.remove(`none`);
+  note_app.classList.add(`qn-none`);
+  maximize_button.classList.remove(`qn-none`);
 });
 
 // maximize functionality
 
 maximize_button.addEventListener(`click`, () => {
-  note_app.classList.remove(`none`);
-  maximize_button.classList.add(`none`);
+  note_app.classList.remove(`qn-none`);
+  maximize_button.classList.add(`qn-none`);
 });
 // dragging functionality
 drag.addEventListener(`mousedown`, mousedown);
@@ -55,21 +54,21 @@ function id_traker() {
 //save button
 function save(e) {
   const para = e.currentTarget.parentElement.parentElement.querySelector(`p`);
-  const expand = e.currentTarget.querySelector(`.expand`);
-  const mini = e.currentTarget.querySelector(`.mini`);
+  const expand = e.currentTarget.querySelector(`.qn-expand`);
+  const mini = e.currentTarget.querySelector(`.qn-mini`);
 
-  if (e.target.classList.contains(`save`)) {
-    alert(`note saved`);
+  if (e.target.classList.contains(`qn-save`)) {
+    alert(`qn-note qn-saved`);
   }
-  if (e.target.classList.contains(`mini`)) {
-    para.classList.add(`none`);
-    mini.classList.add(`none`);
-    expand.classList.remove(`none`);
+  if (e.target.classList.contains(`qn-mini`)) {
+    para.classList.add(`qn-none`);
+    mini.classList.add(`qn-none`);
+    expand.classList.remove(`qn-none`);
   }
-  if (e.target.classList.contains(`expand`)) {
-    para.classList.remove(`none`);
-    mini.classList.remove(`none`);
-    expand.classList.add(`none`);
+  if (e.target.classList.contains(`qn-expand`)) {
+    para.classList.remove(`qn-none`);
+    mini.classList.remove(`qn-none`);
+    expand.classList.add(`qn-none`);
   }
 }
 
@@ -79,18 +78,20 @@ add_button.addEventListener(`click`, () => {
   const title = document.createElement(`h4`);
   const save_button = document.createElement(`div`);
   const text_body = document.createElement(`p`);
+  const placehold = document.createElement(`span`);
   // id track
   const id = id_traker();
   //text content
   const node = document.createTextNode(`Note ${id}`);
-  save_button.innerHTML = `<div class="controls">
-          <img class="save" id="s-${id}" src="./SVGs/save.svg" alt="save button" />
-          <img class="mini" id="m-${id}" src="./SVGs/mini.svg" alt="collapse button" />
-          <img class="expand none" id="e-${id}" src="./SVGs/expand.svg" alt="expend button" />
+  save_button.innerHTML = `<div class="qn-controls">
+          <img class="qn-save" id="s-${id}" src="./SVGs/save.svg" alt="save button" />
+          <img class="qn-mini" id="m-${id}" src="./SVGs/mini.svg" alt="collapse button" />
+          <img class="qn-expand qn-none" id="e-${id}" src="./SVGs/expand.svg" alt="expend button" />
         </div>`;
   const text_body_placeholder = document.createTextNode(
     `This is a note content`
   );
+  placehold.textContent = `l`;
 
   // save_button.setAttribute(`id`, `btn-${id}`);
   note_div.setAttribute(`id`, `${id}`);
@@ -100,6 +101,7 @@ add_button.addEventListener(`click`, () => {
   title_div.appendChild(title);
   title_div.appendChild(save_button);
   text_body.appendChild(text_body_placeholder);
+  note.append(placehold);
 
   title.appendChild(node);
   // save_button.appendChild(button_text);
@@ -109,7 +111,7 @@ add_button.addEventListener(`click`, () => {
   text_body.contentEditable = true;
   title.contentEditable = true;
 
-  title_div.classList.add(`flex-row-spaced`); //display flex
+  title_div.classList.add(`qn-flex-row-spaced`); //display flex
   // save_button.classList.add(`save`);
 
   //expend and collapse
